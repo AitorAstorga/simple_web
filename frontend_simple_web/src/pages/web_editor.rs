ad00ca9b5f4d3d21136927a666ca78dc231ada07
Aitor <a.astorga.sdv@protonmail.com>
2025-07-13 15:18:54 +0200

    Add info text in the header

--- frontend_simple_web/src/pages/web_editor.rs
+++ frontend_simple_web/src/pages/web_editor.rs
@@ -1,6 +1,6 @@
 // frontend_simple_web/src/pages/web_editor.rs
 use yew::prelude::*;
-use crate::components::{file_browser::FileBrowser, code_editor::CodeEditor};
+use crate::{components::{code_editor::CodeEditor, file_browser::FileBrowser}, config_file::get_env_var};
 
 #[function_component(WebEditor)]
 pub fn web_editor() -> Html {
@@ -15,7 +15,10 @@ pub fn web_editor() -> Html {
         <div>
             <header>
                 <img src="static/img/aichan.svg" alt="aichan" class="logo" />
-                <h1 class="font-bold">{ "aichan's Simple Web - Web Editor" }</h1>
+                <div>
+                    <h1 class="font-bold">{ "aichan's Simple Web Editor" }</h1>
+                    <p>{ "Your static site is served at: " }{ get_env_var("API_URL") } { "You can change this with the API_URL env variable." }</p>
+                </div>
             </header>
             <div class="grid grid-cols-4 h-screen">
                 <aside class="col-span-1 p-3 border-r overflow-y-auto">
